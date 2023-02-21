eeaac0faf95a1f3a3d68f6adb983543cebc2c4b7
wacuna <86488461+wacuna1@users.noreply.github.com>
2023-02-20 18:19:48 -0800

    Another method to commit test.

--- TestPython.py
+++ TestPython.py
@@ -3,8 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-url = "C:\Users\willa\My Drive\Schooling\Classes\USD\Spring 23\Probability and Statistics\Week 1\Income.dat"
+url = "C:/Users/willa/My Drive/Schooling/Classes/USD/Spring 23/Probability and Statistics/Week 1/Income.dat"
 data = pd.read_csv(url, delimiter='\s+')
+
+"""
+testing a different commit method"""
 
 plt.hist(data['income'], bins=20)
 plt.xlabel('Income')
